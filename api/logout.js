262757d4290ff1101c388b53bd4987abc639d4f2
pepsimekhi2 <mekhiscreations@gmail.com>
2025-11-14 16:39:31 -0500

    Delete api directory

--- api/logout.js
+++
@@ -1,9 +0,0 @@
-export default async function handler(req, res) {
-    if (req.method !== 'POST') {
-        return res.status(405).json({ error: 'Method not allowed' });
-    }
-
-    res.setHeader('Set-Cookie', 'discord_session=; Path=/; HttpOnly; Secure; SameSite=Strict; Max-Age=0');
-    
-    return res.status(200).json({ success: true });
-}
